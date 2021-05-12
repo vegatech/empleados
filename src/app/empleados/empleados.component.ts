fd9f39849a1aaa254666c55372907d98da892682
vegatech <itservicios23@gmail.com>
2021-05-12 16:37:50 -0500

    se corrigen campos de formulario, boton de guardado en firestore

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmpleadosService } from "../shared/empleados.service";
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
@@ -8,14 +9,33 @@ import { EmpleadosService } from "../shared/empleados.service";
 export class EmpleadosComponent implements OnInit {
 
   constructor(public empleadosService: EmpleadosService) { }
-  coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", "Machiato", "Mocha", "Hot Chocolate", "Tea"];
+  cargos = ["Diseñador", "Pogramador", "Fundador CEO", "Recursos Humanos"];
   ngOnInit(): void {
   }
-  coffeeOrder = [];addCoffee = coffee => this.coffeeOrder.push(coffee);removeCoffee = coffee => {
-    let index = this.coffeeOrder.indexOf(coffee);
-    if (index > -1) this.coffeeOrder.splice(index, 1);};
+  employee = [];addCargo = empleado => this.employee.push(empleado);removeEmployee = employee => {
+    let index = this.employee.indexOf(employee);
+    if (index > -1) this.employee.splice(index, 1);};
 
     onSubmit(){
+      this.empleadosService.form.value.empleado = this.employee;
+      let data = this.empleadosService.form.value;
       
+     this.empleadosService.createEmployeesSave(data)
+         .then((res) => {
+          console.log('This is init method');
+             /*do something here....
+             maybe clear the form or give a success message*/
+             if (res.status === 200) { 
+              console.log('This is init method2');
+              Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'El producto fue agregado',
+                showConfirmButton: false,
+                timer: 1500
+              });
+            }
+         });
+
     }
 }
