d34e64bca0eb3ea5f826f3cfd1531877915bf0c9
vegatech <itservicios23@gmail.com>
2021-05-13 23:21:28 -0500

    se agrega rutas en app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoListComponent } from './empleado-list/empleado-list.component';
+
+const routes: Routes = [
+  { path: '', component: EmpleadoListComponent  },
+  { path: 'home', component: EmpleadoListComponent  },
+  { path: 'new-employee', component: EmpleadosComponent  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/empleado-list/empleado-list.component.ts
+++ src/app/empleado-list/empleado-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmpleadosService } from "../shared/empleados.service";
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-empleado-list',
   templateUrl: './empleado-list.component.html',
@@ -7,7 +8,10 @@ import { EmpleadosService } from "../shared/empleados.service";
 })
 export class EmpleadoListComponent implements OnInit {
 
-  constructor(private empleadosService:EmpleadosService) { }
+  constructor(private empleadosService:EmpleadosService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
   ngOnInit() {
     this.getEmployees();
@@ -25,4 +29,10 @@ export class EmpleadoListComponent implements OnInit {
   /*deleteEmployee = data => this.empleadosService.deleteCoffeeOrder(data);
 
   markCompleted = data => this.empleadosService.updateCoffeeOrder(data);*/
+
+
+  AddUser(){
+    console.log("ingresa boton nuevo empleado");
+    this.router.navigate(['/new-employee']);
+  }
 }
